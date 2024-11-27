4a6603b0e4472ace4de5929162714536f1a2fd01
SostellTODA <sostelltoda@gmail.com>
2024-11-28 00:06:00 +0100

    fonctionnalités ok

--- public/js/home.js
+++ public/js/home.js
@@ -17,16 +17,15 @@ document.addEventListener("DOMContentLoaded", () => {
     const navLinks = document.getElementById("navigation-links");
 
     // Liens selon le rôle de l'utilisateur
+    navLinks.innerHTML += `<li><a href="agenda.html">Agenda</a></li>`;
+    navLinks.innerHTML += `<li><a href="createClient.html">Créer un client</a></li>`;
+
     if (["Superviseur", "Administrateur"].includes(userInfo.role)) {
       navLinks.innerHTML += `<li><a href="manageCampaigns.html">Gérer les campagnes</a></li>`;
     }
     if (userInfo.role === "Administrateur") {
       navLinks.innerHTML += `<li><a href="createProfile.html">Créer un profil utilisateur</a></li>`;
     }
-    navLinks.innerHTML += `
-      <li><a href="agenda.html">Agenda</a></li>
-      <li><a href="createClient.html">Créer un client</a></li>
-    `;
   } else {
     alert("Erreur d'authentification. Veuillez vous reconnecter.");
     window.location.href = "/login.html";
